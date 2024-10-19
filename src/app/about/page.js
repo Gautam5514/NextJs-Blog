4e8195209da135c87ccf41277c61d56d332021f4
Gautam pandit <gautampandit797@gmail.com>
2024-10-20 02:29:13 +0530

    Added some message

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -21,13 +21,11 @@ export default function About() {
                         <div className="w-full md:w-2/3 flex flex-col justify-center ">
                             <h1 className="text-4xl font-bold text-gray-800 dark:text-white mb-4">About Me</h1>
                             <p className="text-gray-600 dark:text-gray-50 text-lg mb-4">
-                                Hello! I'm Gautam Pandit, a software developer and tech enthusiast with experience in full-stack development. I created this blog to share my journey, tips, and tutorials on web technologies, programming languages, and projects I've worked on. I believe continuous learning is key, and I'm here to help others advance their coding skills.
+                                Hello! I&apos;m Gautam Pandit, a software developer and tech enthusiast with experience in full-stack development. I created this blog to share my journey, tips, and tutorials on web technologies, programming languages, and projects I&apos;ve worked on. I believe continuous learning is key, and I&apos;m here to help others advance their coding skills.
                             </p>
                             <p className="text-gray-600 dark:text-gray-50 text-lg">
-                                Whether you're just beginning or aiming to enhance your expertise, you'll find a variety of insights and resources here. Let's dive into the exciting world of development together!
+                                Whether you&apos;re just beginning or aiming to enhance your expertise, you&apos;ll find a variety of insights and resources here. Let&apos;s dive into the exciting world of development together!
                             </p>
-
-
                         </div>
                     </div>
                 </div>
@@ -36,39 +34,60 @@ export default function About() {
             <section className="py-16 bg-gray-50 dark:bg-gray-800 dark:text-white">
                 <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
                     <div className="text-center mb-12">
-                        <h2 className="text-3xl font-extrabold text-gray-900 dark:text-white">Gautam's Journey as a Coder</h2>
+                        <h2 className="text-3xl font-extrabold text-gray-900 dark:text-white">Gautam&apos;s Journey as a Coder</h2>
                         <p className="mt-4 text-lg text-gray-600 dark:text-gray-50">
-                            From curious beginner to seasoned developer, here's how Gautam navigated the world of programming.
+                            From curious beginner to seasoned developer, here&apos;s how Gautam navigated the world of programming.
                         </p>
                     </div>
                     <div className="space-y-12">
                         <div className="flex flex-col md:flex-row items-center">
                             <div className="md:w-1/3">
-                                <img src="/images/devfest.jpg" alt="Harry as a beginner" className="w-full rounded-lg shadow-lg" />
+                                <Image
+                                    src="/images/devfest.jpg"
+                                    alt="Harry as a beginner"
+                                    layout="responsive"
+                                    width={500}
+                                    height={300}
+                                    className="w-full rounded-lg shadow-lg"
+                                />
                             </div>
                             <div className="md:w-2/3 md:pl-8 mt-8 md:mt-0">
-                                <h3 className="text-2xl font-bold text-gray-800 dark:text-white dark:text-white">The Spark of Curiosity</h3>
+                                <h3 className="text-2xl font-bold text-gray-800 dark:text-white">The Spark of Curiosity</h3>
                                 <p className="mt-4 text-gray-600 dark:text-gray-50">
-                                Gautam’s coding journey began in high school when he discovered his first programming language—Python. What started as a simple curiosity quickly transformed into a passion, leading Gautam to dedicate countless hours to experimenting with code, building projects, and mastering software development fundamentals. Now, as a skilled developer, Gautam shares insights and knowledge with others, inspiring the next generation of tech enthusiasts.
+                                    Gautam’s coding journey began in high school when he discovered his first programming language—Python. What started as a simple curiosity quickly transformed into a passion, leading Gautam to dedicate countless hours to experimenting with code, building projects, and mastering software development fundamentals. Now, as a skilled developer, Gautam shares insights and knowledge with others, inspiring the next generation of tech enthusiasts.
                                 </p>
                             </div>
                         </div>
 
                         <div className="flex flex-col md:flex-row-reverse items-center">
                             <div className="md:w-1/3">
-                                <img src="/images/devfest.jpg" alt="Harry learning new skills" className="w-full rounded-lg shadow-lg" />
+                                <Image
+                                    src="/images/devfest.jpg"
+                                    alt="Harry learning new skills"
+                                    layout="responsive"
+                                    width={500}
+                                    height={300}
+                                    className="w-full rounded-lg shadow-lg"
+                                />
                             </div>
                             <div className="md:w-2/3 md:pr-8 mt-8 md:mt-0">
                                 <h3 className="text-2xl font-bold text-gray-800 dark:text-white">Diving Deeper</h3>
                                 <p className="mt-4 text-gray-600 dark:text-gray-50">
-                                    After mastering the basics, Gautam thirst for knowledge grew. He began exploring more complex topics such as data structures, algorithms, and web development. Enrolling in online courses and attending coding bootcamps, Gautam quickly expanded his skill set, taking on freelance projects to apply his knowledge in real-world scenarios.
+                                    After mastering the basics, Gautam&apos;s thirst for knowledge grew. He began exploring more complex topics such as data structures, algorithms, and web development. Enrolling in online courses and attending coding bootcamps, Gautam quickly expanded his skill set, taking on freelance projects to apply his knowledge in real-world scenarios.
                                 </p>
                             </div>
                         </div>
 
                         <div className="flex flex-col md:flex-row items-center">
                             <div className="md:w-1/3">
-                                <img src="/images/devfest.jpg" alt="Harry working on a big project" className="w-full rounded-lg shadow-lg" />
+                                <Image
+                                    src="/images/devfest.jpg"
+                                    alt="Harry working on a big project"
+                                    layout="responsive"
+                                    width={500}
+                                    height={300}
+                                    className="w-full rounded-lg shadow-lg"
+                                />
                             </div>
                             <div className="md:w-2/3 md:pl-8 mt-8 md:mt-0">
                                 <h3 className="text-2xl font-bold text-gray-800 dark:text-white">Taking on Challenges</h3>
@@ -78,9 +97,17 @@ export default function About() {
                             </div>
                         </div>
 
-                        {/* <div className="flex flex-col md:flex-row-reverse items-center">
+                        {/* Uncomment this section if needed
+                        <div className="flex flex-col md:flex-row-reverse items-center">
                             <div className="md:w-1/3">
-                                <img src="/images/devfest.jpg" alt="Harry mentoring others" className="w-full rounded-lg shadow-lg" />
+                                <Image
+                                    src="/images/devfest.jpg"
+                                    alt="Harry mentoring others"
+                                    layout="responsive"
+                                    width={500}
+                                    height={300}
+                                    className="w-full rounded-lg shadow-lg"
+                                />
                             </div>
                             <div className="md:w-2/3 md:pr-8 mt-8 md:mt-0">
                                 <h3 className="text-2xl font-bold text-gray-800 dark:text-white">Giving Back</h3>
@@ -92,7 +119,6 @@ export default function About() {
                     </div>
                 </div>
             </section>
-
         </div>
     );
 }
